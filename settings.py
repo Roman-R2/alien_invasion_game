243024e6079ea6b2315e3fb852553e7e52aea69f
Roman <slyusarrv@yandex.ru>
2021-10-06 16:24:10 +0500

    some edits

--- settings.py
+++ settings.py
@@ -20,6 +20,6 @@ class Settings:
 
         # Настройки пришельцев
         self.alien_speed = 0.2
-        self.fleet_drop_speed = 10
+        self.fleet_drop_speed = 0.2
         # fleet_direction = 1 обозначает движение вправо; а -1 - влево.
         self.fleet_direction = 1
