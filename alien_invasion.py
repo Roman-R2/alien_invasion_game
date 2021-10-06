3501c1379a0f67173e9b7e9da13f85d2ae8a0f38
Roman <slyusarrv@yandex.ru>
2021-10-06 10:12:57 +0500

    first commit

---
+++ alien_invasion.py
@@ -0,0 +1,61 @@
+import sys
+
+import pygame
+
+from alien_invasion_game.ship import Ship
+from settings import Settings
+
+
+class AlienInvasion:
+    """Класс для управления ресурсами и поведением игры"""
+
+    def __init__(self):
+        """Инициализирует игру и создает игровые ресурсы"""
+        pygame.init()
+
+        self.settings = Settings()
+        # Установим размер окна
+        self.screen = pygame.display.set_mode(
+            (
+                self.settings.screen_width,
+                self.settings.screen_height))
+        pygame.display.set_caption("Alien Invasion")
+
+        self.ship = Ship(self)
+
+    def run_game(self):
+        """Запуск основного цикла игры"""
+        while True:
+            self._check_events()
+            self.ship.update()
+            self._update_screen()
+
+    def _check_events(self):
+        # Отслеживание событий клавиатуры и мыши
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                sys.exit()
+            elif event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_RIGHT:
+                    self.ship.moving_right = True
+                if event.key == pygame.K_LEFT:
+                    self.ship.moving_left = True
+            elif event.type == pygame.KEYUP:
+                if event.key == pygame.K_RIGHT:
+                    self.ship.moving_right = False
+                if event.key == pygame.K_LEFT:
+                    self.ship.moving_left = False
+
+    def _update_screen(self):
+        # При каждом проходе цикла перерисовывается экран
+        self.screen.fill(self.settings.bgcolor)
+        self.ship.blitme()
+
+        # Отображение последнего прорисованного экрана
+        pygame.display.flip()
+
+
+if __name__ == "__main__":
+    # Создание экземпляра и запуск игры
+    ai = AlienInvasion()
+    ai.run_game()
